3a726a396dfd8492bc86956779e97de385253732
Alejandro Hernández <alejandrehl@icloud.com>
2023-02-13 15:50:30 -0300

    save

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Test</title>
-</head>
-<body>
-	<p>
-		@foreach (\Session::get('cart') as $key => $value)
-			<strong>{{ $key }}</strong>: {{ $value }} <br>
-		@endforeach
-	</p>
 
-	<form action="{{ route('webpay.init') }}" method="post">
-		{{ csrf_field() }}
-		<button type="submit">Comprar</button>
-	</form>
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+
+<body>
+    <p>
+        @foreach (\Session::get('cart') as $key => $value)
+            <strong>{{ $key }}</strong>: {{ $value }} <br>
+        @endforeach
+    </p>
+
+    <form action="{{ route('webpay.init') }}" method="post">
+        {{ csrf_field() }}
+        <button type="submit">Comprar</button>
+    </form>
 </body>
+
 </html>
